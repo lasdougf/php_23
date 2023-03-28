7754a3e70098ba9f26aca54eddb1836fd00e4702
lasdougf <122946390+lasdougf@users.noreply.github.com>
2023-03-28 20:26:23 -0300

    Create exercicio.php

---
+++ exercicio.php
@@ -0,0 +1,64 @@
+
+<?php
+
+const CPF = '1234567820'; // Nunca muda o valor
+$idade = "10"; //p pode mudar
+
+$numero = 7; // testar 50 - 30000
+
+
+
+// 1-) Verificar se o numero informado é par.
+
+
+// $resultado = 10 / 2; // 5 - > quociente
+$resto = $numero % 2; // 0 - > resto da divisao (operador MOD)
+
+if ($resto ==0) {
+    echo "<br>O numero é PAR: " . $numero; // 50, 52 , 54 ... 
+    }else{ 
+        echo "<br>O numero é IMPAR: " .$numero; // 51, 53 , 55 ... 
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
